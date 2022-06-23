e606a8caa287834ce51cd182bef3d45430847582
hungdn <hungdn@runsystem.net>
2022-06-23 21:28:26 +0700

    .

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react';
-import './loginpage.scss';
-import { ReactComponent as Banner2Ic } from '../../Assets/banner2.svg';
-import Banner from '../../Components/Banner/Banner';
-import { Outlet, useNavigate } from 'react-router';
+import React, { useEffect } from "react";
+import "./loginpage.scss";
+import Banner from "../../Components/Banner/Banner";
+import { Outlet, useNavigate } from "react-router";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+
   return (
-    <div className='app__login-page'>
+    <div className="app__login-page">
       <Outlet />
       <Banner />
     </div>
