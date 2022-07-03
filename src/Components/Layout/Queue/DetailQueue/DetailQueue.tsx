aa7f1698dbbd84bd79b28d62d5cd6445f86573f9
hungdn <hungdn@runsystem.net>
2022-07-04 00:39:19 +0700

    z

--- src/Components/Layout/Queue/DetailQueue/DetailQueue.tsx
+++ src/Components/Layout/Queue/DetailQueue/DetailQueue.tsx
@@ -1,31 +1,59 @@
-import React from 'react';
-import './detailqueue.scss';
-import { useNavigate } from 'react-router';
-import { ReactComponent as ReturnIc } from '../../../../Assets/return.svg';
+import React, { useLayoutEffect, useState } from "react";
+import "./detailqueue.scss";
+import { useNavigate } from "react-router";
+import { ReactComponent as ReturnIc } from "../../../../Assets/return.svg";
+import { useSearchParams } from "react-router-dom";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../../firebase";
 
 const DetailQueue: React.FC = () => {
   const navigate = useNavigate();
+  const [search, setSearch] = useSearchParams();
+  const [item, setItem] = useState({
+    no: "",
+    customerName: "",
+    serviceName: "",
+    grantTime: "",
+    expireTime: "",
+    queueStatus: "",
+    provideBy: "",
+  });
+
+  useLayoutEffect(() => {
+    const id = search.get("id");
+    const docRef = doc(db, "queueList", id || "");
+
+    onSnapshot(docRef, (doc: any) => {
+      setItem({
+        ...doc.data(),
+      });
+    });
+  }, []);
   return (
     <div className="app__layout-queue__detail-queue">
       <div className="app__layout-queue__detail-queue__title">
         Quản lý cấp số
       </div>
       <div className="app__layout-queue__detail-queue__container">
-        <div className="row form-label">Thông tin thiết bị</div>
+        <div className="row form-label">Thông tin Cấp số</div>
         <div className="row app__layout-queue__detail-queue__container__label">
-          <div className="flex-1">Mã thiết bị:</div>
-          <div className="flex-1">Loại thiết bị:</div>
+          <div className="flex-1">Họ tên: {item.customerName}</div>
+          <div className="flex-1">Nguồn cấp: {item.provideBy}</div>
         </div>
         <div className="row app__layout-queue__detail-queue__container__label">
-          <div className="flex-1">Tên thiết bị:</div>
-          <div className="flex-1">Tên đăng nhập:</div>
+          <div className="flex-1">Tên dịch vụ: {item.serviceName}</div>
+          <div className="flex-1">Trạng thái: {item.queueStatus}</div>
         </div>
         <div className="row app__layout-queue__detail-queue__container__label">
-          <div className="flex-1">Địa chỉ IP:</div>
-          <div className="flex-1">Mật khẩu:</div>
+          <div className="flex-1">Số thứ tự: {item.no}</div>
+          <div className="flex-1">Số điện thoại:</div>
         </div>
         <div className="row app__layout-queue__detail-queue__container__label">
-          <div className="flex-1">Dịch vụ sử dụng:</div>
+          <div className="flex-1">Thời gian cấp: {item.grantTime}</div>
+          <div className="flex-1">Địa chỉ Email:</div>
+        </div>
+        <div className="row app__layout-queue__detail-queue__container__label">
+          <div className="flex-1">Hạn sử dụng: {item.expireTime}</div>
         </div>
       </div>
     </div>
